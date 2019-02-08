105beed8e82b55b330b4825588b0dffcd7772117
wagnel <wagnelcuello@gmail.com>
2019-02-07 22:40:29 -0400

    Agregar archivos de proyecto.

---
+++ DiplomadoMVC-Crud-HTML-Helps-NoTipados/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DiplomadoMVC_Crud_HTML_Helps_NoTipados
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
